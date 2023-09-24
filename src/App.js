141c772f1513e3cac094b0d012c380d69b169099
Fraser Dwyer <fraserd1413@gmail.com>
2023-09-24 10:45:58 +0100

    Rough layout added to webpage (mobile and desktop)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import piggypayTitle from "./Images/piggypay.png";
 
 function App() {
   return (
-    <div className="banner">
-      <img src={piggypayTitle} alt="piggypay"></img>
+    <div>
+      <div className="banner">
+        <img src={piggypayTitle} alt="piggypay"></img>
+      </div>
+      <div className="background">
+        <div className="content">
+          <div className="output">
+            <h2>You have earned</h2>
+            <h1>£5.52</h1>
+            <h2>since 09:04</h2>
+          </div>
+          <div className="input">
+            <p>Picture of pig goes here</p>
+            <p>Time started:</p>
+            <p>Salary:</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
