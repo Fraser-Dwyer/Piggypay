9e45a15e904a19dd4bc695ce0585bd48b68a5cc3
Fraser Dwyer <fraserd1413@gmail.com>
2023-10-13 19:16:47 +0100

    Money calculation algorithm hmmm yes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import piggypayTitle from "./Images/piggypay.png";
 import piggyGif from "./Images/piggyGIF5.gif";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import buttonOink from "./Audio/oink.mp3";
 import daddyOink from "./Audio/daddyOink.mp3";
 
 function App() {
   const [startTime, setStartTime] = useState("");
   const [displayTime, setDisplayTime] = useState("");
-  const [salary, setSalary] = useState("");
+  const [salary, setSalary] = useState("0");
+  const [salaryOption, setSalaryOption] = useState("Per annum");
+  const [time, setTime] = useState(new Date());
 
   const handleSetTime = (e) => {
     e.preventDefault();
@@ -19,13 +21,35 @@ function App() {
   const handleSetSalary = (e) => {
     e.preventDefault();
     new Audio(buttonOink).play();
-    // Work out seconds wage here
+    var currentHour = time.getHours();
+    var currentMin = time.getMinutes();
+    var startHour = parseInt(startTime.slice(0, 2));
+    var startMin = parseInt(startTime.slice(3, 5));
+    var totalMinsWorked =
+      60 - startMin + 60 * (currentHour - (startHour + 1)) + currentMin;
+    console.log(totalMinsWorked);
+    var salaryPerMin;
+    if (salaryOption === "Per annum") {
+      salaryPerMin = parseInt(salary) / (52 * 37.5 * 60);
+    } else {
+      salaryPerMin = parseInt(salary) / 60;
+    }
+    var totalMoneyEarned = salaryPerMin * totalMinsWorked;
+    alert(totalMoneyEarned);
   };
 
   const handleDaddyOink = (e) => {
     e.preventDefault();
     new Audio(daddyOink).play();
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div>
@@ -64,9 +88,14 @@ function App() {
                 value={salary}
                 onChange={(e) => setSalary(e.target.value)}
               />
-              <select id="salaryFrequency" name="salaryFrequency">
-                <option value="hourly">Per hour</option>
+              <select
+                id="salaryFrequency"
+                name="salaryFrequency"
+                value={salaryOption}
+                onChange={(e) => setSalaryOption(e.target.value)}
+              >
                 <option value="yearly">Per annum</option>
+                <option value="hourly">Per hour</option>
               </select>
               <button onClick={(e) => handleSetSalary(e)}>••</button>
             </form>
