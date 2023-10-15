b3296ef9b01b827b4d1ca36d03d54bcbfef996ce
Fraser Dwyer <fraserd1413@gmail.com>
2023-10-15 18:44:36 +0100

    Working salary calculation

--- src/App.js
+++ src/App.js
@@ -8,34 +8,53 @@ import daddyOink from "./Audio/daddyOink.mp3";
 function App() {
   const [startTime, setStartTime] = useState("");
   const [displayTime, setDisplayTime] = useState("");
-  const [salary, setSalary] = useState("0");
+  const [salary, setSalary] = useState();
   const [salaryOption, setSalaryOption] = useState("Per annum");
   const [time, setTime] = useState(new Date());
+  const [totalEarned, setTotalEarned] = useState("0.00");
+  const [frames, setFrames] = useState(0);
+  const [mons, setMons] = useState(0);
 
   const handleSetTime = (e) => {
     e.preventDefault();
     setDisplayTime(startTime);
     new Audio(buttonOink).play();
+    handleCalculation();
   };
 
   const handleSetSalary = (e) => {
     e.preventDefault();
     new Audio(buttonOink).play();
-    var currentHour = time.getHours();
-    var currentMin = time.getMinutes();
-    var startHour = parseInt(startTime.slice(0, 2));
-    var startMin = parseInt(startTime.slice(3, 5));
-    var totalMinsWorked =
-      60 - startMin + 60 * (currentHour - (startHour + 1)) + currentMin;
-    console.log(totalMinsWorked);
-    var salaryPerMin;
-    if (salaryOption === "Per annum") {
-      salaryPerMin = parseInt(salary) / (52 * 37.5 * 60);
+    handleCalculation();
+  };
+
+  const handleCalculation = () => {
+    if (salary !== null && displayTime !== null && salary > 0) {
+      setTime(new Date());
+      var currentHour = time.getHours();
+      var currentMin = time.getMinutes();
+      var currentSec = time.getSeconds();
+      var startHour = parseInt(startTime.slice(0, 2));
+      var startMin = parseInt(startTime.slice(3, 5));
+
+      var salaryPerSecond;
+      if (salaryOption === "Per annum") {
+        salaryPerSecond = parseInt(salary) / (52 * 37.5 * 60 * 60);
+      } else {
+        salaryPerSecond = parseInt(salary) / 3600;
+      }
+
+      var minutesWorked = Math.abs(currentMin - startMin);
+      var hoursWorked = Math.abs(currentHour - startHour);
+      var totalSecsWorked =
+        currentSec + 60 * minutesWorked + 3600 * hoursWorked;
+      setMons(totalSecsWorked * salaryPerSecond);
+      setTotalEarned((totalSecsWorked * salaryPerSecond).toFixed(2));
+      var frameRate = 1000 / (salaryPerSecond * 100);
+      setFrames(frameRate);
     } else {
-      salaryPerMin = parseInt(salary) / 60;
+      setFrames(0);
     }
-    var totalMoneyEarned = salaryPerMin * totalMinsWorked;
-    alert(totalMoneyEarned);
   };
 
   const handleDaddyOink = (e) => {
@@ -45,11 +64,14 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(new Date());
-    }, 5000);
+      if (frames > 0) {
+        setMons(mons + 0.01);
+      }
+      setTotalEarned(mons.toFixed(2));
+    }, frames);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [frames, mons]);
 
   return (
     <div>
@@ -60,8 +82,9 @@ function App() {
         <div className="content">
           <div className="output">
             <h2>You've earned</h2>
-            <h1>£5.52</h1>
-            <h2>since {displayTime}</h2>
+            <h1>£{totalEarned}</h1>
+            {displayTime && <h2>since {displayTime}</h2>}
+            {!displayTime && <h2>today! :(</h2>}
           </div>
           <div className="input">
             <img
